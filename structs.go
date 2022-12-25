15e21bac7d7a6458ffdcf1260a104fb794cab2f5
Oğuz Halit Sak <oguzhalit.sak@gmail.com>
2022-12-25 03:11:45 +0300

    structs Example

---
+++ structs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
+func main() {
+	fmt.Println(person{"Oguz", 20})
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{name: "Ann", age: 40})
+	fmt.Println(newPerson("Jon"))
+}
