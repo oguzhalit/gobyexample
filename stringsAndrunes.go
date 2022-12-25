c55ab7eb37301ac1546c9dce1e5e4a07c035348f
Oğuz Halit Sak <oguzhalit.sak@gmail.com>
2022-12-25 03:01:03 +0300

    Strings and Runes Example

---
+++ stringsAndrunes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	const s = "สวัสดี"
+
+	fmt.Println("Len:", len(s))
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+	fmt.Println("Rune Count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+}
